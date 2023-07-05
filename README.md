81c1aadb16f3fa443d9da0fce2fce8c346536f9f
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-05 15:33:43 +0900

    lineTo 기능, lineWidth 조절, color 변경 기능 구현

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# VanillaJS로 만든 Canvus(그림판) App입니다.
+# VanillaJS로 만든 DrawingBoard(그림판) App입니다.
---
+++ js/app.js
@@ -0,0 +1,46 @@
+const lineWidth = document.querySelector("#line-width");
+const color = document.querySelector("#color");
+const canvas = document.querySelector("canvas");
+canvas.width = 800;
+canvas.height = 800;
+
+const ctx = canvas.getContext("2d");
+ctx.lineWidth = lineWidth.value;
+
+let isPainting = false;
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+  isPainting = true;
+}
+
+function cancelPainting() {
+  isPainting = false;
+  ctx.beginPath();
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+lineWidth.addEventListener("change", onLineWidthChange);
+
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+color.addEventListener("change", onColorChange);
