5cb9ca3ad97e5cd1d7c926276d423bf505c4ab9a
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-05 15:48:22 +0900

    colorOption 추가

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const colorOptions = Array.from(document.querySelectorAll(".color-option"));
 const lineWidth = document.querySelector("#line-width");
 const color = document.querySelector("#color");
 const canvas = document.querySelector("canvas");
@@ -26,7 +27,6 @@ function cancelPainting() {
   isPainting = false;
   ctx.beginPath();
 }
-
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -35,12 +35,18 @@ canvas.addEventListener("mouseleave", cancelPainting);
 function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
-
 lineWidth.addEventListener("change", onLineWidthChange);
 
 function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
   ctx.fillStyle = event.target.value;
 }
-
 color.addEventListener("change", onColorChange);
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
